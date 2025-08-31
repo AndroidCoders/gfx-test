a23efad4d80c21e3fdd1fcc8d0500e0452e1d27e
Espen <espen.walderhaug@gmail.com>
2025-08-31 03:59:58 +0200

    feat: Implement SDL fullscreen canvas POC

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,48 @@
+extern crate sdl2;
+
 pub mod game_state;
 pub mod renderer;
 
-fn main() {
-    println!("Hello, world!");
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?; // TODO: Error handling
+    let video_subsystem = sdl_context.video()?; // TODO: Error handling
+
+    let window = video_subsystem.window(
+        "gfx-test",
+        1920, // TODO: Use constants for screen dimensions
+        1080
+    )
+    .fullscreen_desktop() // Set to fullscreen
+    .build()?; // TODO: Error handling
+
+    let mut canvas = window.into_canvas().build()?; // TODO: Error handling
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0)); // Black background
+
+    let mut event_pump = sdl_context.event_pump()?; // TODO: Error handling
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} => {
+                    break 'running
+                },
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+
+        // Clear the screen
+        canvas.clear();
+        // Present the changes to the screen
+        canvas.present();
+    }
+
+    Ok(())
 }
